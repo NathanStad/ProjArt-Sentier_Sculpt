19959915faa566f4052fa66a486940d7d6351426
Sami Gilliand <sami.gilliand@heig-vd.ch>
2024-05-30 09:53:11 +0200

    Modification des localisations des étapes des sentiers à Lausanne et au Sentier

--- database/seeders/EtapeSeeder.php
+++ database/seeders/EtapeSeeder.php
@@ -11,7 +11,6 @@ use Faker\Factory as Faker;
 class EtapeSeeder extends Seeder {
     public function run(): void
     {
-        // Étapes pour le premier sentier (comme déjà fait précédemment)
         $sentier1 = Sentier::find(1);
         $this->createEtapesForSentier1($sentier1);
 
@@ -29,12 +28,12 @@ class EtapeSeeder extends Seeder {
         $latitude1 = 46.778091603482416;
         $longitude1 = 6.634807457364027;
 
-        $latitude2 = 46.7845; // Coordonnées ajustées
+        $latitude2 = 46.7845; 
         $longitude2 = 6.6442;
         $distance1 = $this->haversineGreatCircleDistance($latitude1, $longitude1, $latitude2, $longitude2);
         $duree1 = ($distance1 / 5) * 3600;
 
-        $latitude3 = 46.77887821612798; // Coordonnées ajustées
+        $latitude3 = 46.77887821612798; 
         $longitude3 = 6.64853209607303;
         $distance2 = $this->haversineGreatCircleDistance($latitude2, $longitude2, $latitude3, $longitude3);
         $duree2 = ($distance2 / 5) * 3600;
@@ -81,22 +80,14 @@ class EtapeSeeder extends Seeder {
         $latitude1 = 46.52700585711486;
         $longitude1 = 6.6116923896493995;
 
-        // Générer des coordonnées pour l'étape 2 entre 1 km et 2.5 km de la première étape
-        do {
-            $latitude2 = $latitude1 + (rand(-25000, 25000) / 1000000);
-            $longitude2 = $longitude1 + (rand(-25000, 25000) / 1000000);
-            $distance1 = $this->haversineGreatCircleDistance($latitude1, $longitude1, $latitude2, $longitude2);
-        } while ($distance1 < 1 || $distance1 > 2.5);
-
+        $latitude2 = 46.5368; // Coordonnées ajustées
+        $longitude2 = 6.6257;
+        $distance1 = $this->haversineGreatCircleDistance($latitude1, $longitude1, $latitude2, $longitude2);
         $duree1 = ($distance1 / 5) * 3600;
 
-        // Générer des coordonnées pour l'étape 3 entre 1 km et 2.5 km de la deuxième étape
-        do {
-            $latitude3 = $latitude2 + (rand(-25000, 25000) / 1000000);
-            $longitude3 = $longitude2 + (rand(-25000, 25000) / 1000000);
-            $distance2 = $this->haversineGreatCircleDistance($latitude2, $longitude2, $latitude3, $longitude3);
-        } while ($distance2 < 1 || $distance2 > 2.5);
-
+        $latitude3 = 46.5412; // Coordonnées ajustées
+        $longitude3 = 6.6321;
+        $distance2 = $this->haversineGreatCircleDistance($latitude2, $longitude2, $latitude3, $longitude3);
         $duree2 = ($distance2 / 5) * 3600;
 
         Etape::create([
@@ -141,22 +132,14 @@ class EtapeSeeder extends Seeder {
         $latitude1 = 46.60920005752568;
         $longitude1 = 6.248820104351553;
 
-        // Générer des coordonnées pour l'étape 2 entre 1 km et 2.5 km de la première étape
-        do {
-            $latitude2 = $latitude1 + (rand(-25000, 25000) / 1000000);
-            $longitude2 = $longitude1 + (rand(-25000, 25000) / 1000000);
-            $distance1 = $this->haversineGreatCircleDistance($latitude1, $longitude1, $latitude2, $longitude2);
-        } while ($distance1 < 1 || $distance1 > 2.5);
-
+        $latitude2 = 46.619473;
+        $longitude2 = 6.265666;
+        $distance1 = $this->haversineGreatCircleDistance($latitude1, $longitude1, $latitude2, $longitude2);
         $duree1 = ($distance1 / 5) * 3600;
 
-        // Générer des coordonnées pour l'étape 3 entre 1 km et 2.5 km de la deuxième étape
-        do {
-            $latitude3 = $latitude2 + (rand(-25000, 25000) / 1000000);
-            $longitude3 = $longitude2 + (rand(-25000, 25000) / 1000000);
-            $distance2 = $this->haversineGreatCircleDistance($latitude2, $longitude2, $latitude3, $longitude3);
-        } while ($distance2 < 1 || $distance2 > 2.5);
-
+        $latitude3 = 46.625705;
+        $longitude3 = 6.279295;
+        $distance2 = $this->haversineGreatCircleDistance($latitude2, $longitude2, $latitude3, $longitude3);
         $duree2 = ($distance2 / 5) * 3600;
 
         Etape::create([
@@ -196,10 +179,8 @@ class EtapeSeeder extends Seeder {
         ]);
     }
 
-    // Fonction de calcul de la distance en utilisant la formule Haversine
     private function haversineGreatCircleDistance($latitudeFrom, $longitudeFrom, $latitudeTo, $longitudeTo, $earthRadius = 6371)
     {
-        // Convertir les degrés en radians
         $latFrom = deg2rad($latitudeFrom);
         $lonFrom = deg2rad($longitudeFrom);
         $latTo = deg2rad($latitudeTo);
